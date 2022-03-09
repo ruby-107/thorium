e47624068d69271fdc465b0a0d88382bc9658a31
Manish-162403 <naamcool420@gmail.com>
2022-03-10 00:57:50 +0530

    update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const token1= require("../middleware/auth")
+const jwt = require("jsonwebtoken");
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -11,10 +14,16 @@ router.post("/users", userController.createUser)
 router.post("/login", userController.loginUser)
 
 //The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
+router.get("/users/:userId",token1.tokenCheck,token1.authorise, userController.getUserData)
 
-router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
+router.post("/users/:userId/posts",token1.tokenCheck,token1.authorise, userController.postMessage)
+
+router.put("/users/:userId",token1.tokenCheck,token1.authorise, userController.updateUser)
+
+router.delete('/users/:userId',token1.tokenCheck,token1.authorise, userController.deleteUser)
 
 module.exports = router;
+
+
+// token1.tokenCheck
+// token1.authorise
