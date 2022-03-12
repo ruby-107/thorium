1d3fd1a352b63ff7f82e6581acb82e118176eb59
ruby gupta <ruby45554@gmail.com>
2022-03-12 15:53:29 +0530

    axios

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -1,5 +1,6 @@
-let axios = require("axios")
 
+
+let axios = require("axios")
 
 let getStates = async function (req, res) {
 
@@ -22,10 +23,12 @@ let getStates = async function (req, res) {
 
 let getDistricts = async function (req, res) {
     try {
-        let id = req.params.stateId
+        let district_id = req.query.district_id
+        let date = req.query.date
+       //let district= req.query.district_id
         let options = {
             method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/admin/location/districts/${id}`
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${district_id}&date=${date}`
         }
         let result = await axios(options);
         console.log(result)
@@ -45,8 +48,8 @@ let getByPin = async function (req, res) {
         console.log(`query params are: ${pin} ${date}`)
         var options = {
             method: "get",
-            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
-        }
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}` 
+              }
         let result = await axios(options)
         console.log(result.data)
         res.status(200).send({ msg: result.data })
@@ -57,10 +60,32 @@ let getByPin = async function (req, res) {
     }
 }
 
+// let getOtp = async function (req, res) {
+//     try {
+//         let blahhh = req.body
+        
+//         console.log(`body is : ${blahhh} `)
+//         var options = {
+//             method: "post",
+//             url: `https://cdn-api.co-vin.in/api/v2/auth/public/getOTP`,
+//             data: blahhh
+//         }
+
+//         let result = await axios(options)
+//         console.log(result.data)
+//         res.status(200).send({ msg: result.data })
+//     }
+//     catch (err) {
+//         console.log(err)
+//         res.status(500).send({ msg: err.message })
+//     }
+//}
+
+
 let getOtp = async function (req, res) {
     try {
         let blahhh = req.body
-        
+
         console.log(`body is : ${blahhh} `)
         var options = {
             method: "post",
@@ -79,6 +104,53 @@ let getOtp = async function (req, res) {
 }
 
 
+
+let  getSortCities =  async function (req,res){
+     try{
+
+    let  cities =    ["Bengaluru","Mumbai", "Delhi", "Kolkata", "Chennai", "London", "Moscow"]
+    let cityArray = []
+    for(let i=0; i<cities.length; i++){
+        let obj = { city : cities[i]}
+     let resp = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=2e44a80599463796275ae042c5d18f4b`) 
+    console.log(resp.data.main.temp)
+
+    obj.temp = resp.data.main.temp
+    cityArray.push(obj)
+    }
+    let sorted = cityArray.sort( function(a,b) {return a.temp - b.temp})
+    console.log(sorted)
+    res.status(200).send({status : true, data: sorted})
+}  catch(error){
+    res.status(500).send({status: false, msg: "server error"})
+}
+}
+let createMeme = async function (req, res) {
+
+    try {
+        let  = req.query.q
+        let appid = req.query.appid
+        let options = {
+            method: 'post',
+            url: 'https://api.imgflip.com/caption_image?template_id=87743020&text0=$({text0})&text1=$({text1})&username=chewie12345&password=meme@123'
+        }
+        let result = await axios(options);
+        console.log(result)
+        let data = result.data
+        res.status(200).send({ msg: data, status: true })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+module.exports.createMeme=createMeme
+  
+
+
+
+module.exports.getSortCities = getSortCities
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
--- src/routes/route.js
+++ src/routes/route.js
@@ -10,12 +10,22 @@ router.get("/test-me", function (req, res) {
 
 
 router.get("/cowin/states", CowinController.getStates)
-router.get("/cowin/districtsInState/:stateId", CowinController.getDistricts)
+router.get("/cowin/findByDistrict", CowinController.getDistricts)
 router.get("/cowin/getByPin", CowinController.getByPin)
 
 router.post("/cowin/getOtp", CowinController.getOtp)
 
+
+
+
+
+
+
 // WRITE A GET API TO GET THE LIST OF ALL THE "vaccination sessions by district id" for any given district id and for any given date
+
+
+router.get("/getSortCities/weather", CowinController.getSortCities)
+router.get("/createMeme", CowinController.createMeme)
 
 
 
